d94bc6bf556154153f426d010bcd01a3d4d8db50
JoaoEduardo882 <jooaoeeduardo@gmail.com>
2025-02-03 01:06:32 +0000

    criando variaveis das cidades

---
+++ Super_Trunfo nivel novato.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+#define NUM_ESTADOS 8
+#define NUM_CIDADES 4
+
+int main(){
+    //Variaveis para armazenar os dados das cidades
+    char codigo[4];            //Codigo da cidade (ex: A01, B02)
+    char nome[50];             //Nome da cidade
+    int populacao;             //População da cidade
+    float area;                //Area da cidade em km²
+    float pib;                 //PIB da população em bilhões
+    int pontos_turisticos;     //Numero de Pontos Turisticos
+    char estado;               //Letra do Estado (A, B, C, ..., H)
+    int cidadeNum;             //Numero da Cidade (1, 2, 3, 4)
+    int i, j;
+
+    //Loop para Cadastrar cidades de A a H, e de 1 a 4
+    for (i=0; i < NUM_ESTADOS; i++) {
+        estado = 'A' + i //Atribui a Letra do estado (A, B, C, ..., H)
+    }
+
+
+
+}
